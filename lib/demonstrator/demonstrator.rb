a327bb437a4ce4794d26c4ff9c3a9d56d2c51e43
Jay Pfaffman <pfaffman@gmail.com>
2021-08-24 15:15:40 -0700

    oops

--- lib/demonstrator/demonstrator.rb
+++ lib/demonstrator/demonstrator.rb
@@ -77,7 +77,7 @@ class Demonstrator
   def self.remove_missing_id(ids)
     @process_log += "\n\n###Removing Users\n"
     demonstrator_ids = ids.map { |i| i[:id] }
-    @process_log += "IDS: #{demonstrator_ids"
+    @process_log += "IDS: #{demonstrator_ids}"
     manager_group = Group.find_by_name(SiteSetting.demonstrator_manager_group)
     removed_group = Group.find_by_name(SiteSetting.demonstrator_removed_group)
     demo_group = Group.find_by_name(SiteSetting.demonstrator_group)
