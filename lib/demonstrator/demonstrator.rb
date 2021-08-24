128dbcfe86206390dea0368147923ccc606f3abd
Jay Pfaffman <pfaffman@gmail.com>
2021-08-24 15:32:49 -0700

    syntax

--- lib/demonstrator/demonstrator.rb
+++ lib/demonstrator/demonstrator.rb
@@ -77,7 +77,7 @@ class Demonstrator
 
   def self.remove_missing_id(ids)
     @process_log += "\n\n###Removing Users\n"
-    demonstrator_ids = ids.map { |i| i[:id].to_i }
+    demonstrator_ids = ids.map { |i| (i[:id]).to_i }
     Rails.logger.error("demonstrator_ids: #{demonstrator_ids}")
     @process_log += "IDS: #{demonstrator_ids}"
     manager_group = Group.find_by_name(SiteSetting.demonstrator_manager_group)
